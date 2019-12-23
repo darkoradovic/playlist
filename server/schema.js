893a05542a72dd6df0ced0be809ad3e5649113bb
Darko <darkoradovic1994@gmail.com>
2019-12-23 14:23:36 +0100

    mutations

--- server/schema.js
+++ server/schema.js
@@ -5,40 +5,60 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLBoolean, 
+  GraphQLBoolean,
+  GraphQLList,
   GraphQLID
 } = require("graphql");
 
-// dummy data
+const Book = require("./models/Book");
+const Author = require("./models/Author");
+
+/* // dummy data
 var books = [
-  { name: "Name of the Wind", genre: "Fantasy", id: "1" },
-  { name: "The Final Empire", genre: "Fantasy", id: "2" },
-  { name: "The Long Earth", genre: "Sci-Fi", id: "3" }
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorid: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorid: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "4", authorid: "3" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorid: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorid: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorid: "3" }
 ];
 
 var authors = [
-    { name: 'Patrick Rothfuss', age: 44, id: '1' },
-    { name: 'Brandon Sanderson', age: 42, id: '2' },
-    { name: 'Terry Pratchett', age: 66, id: '3' }
-];
+  { name: "Patrick Rothfuss", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Pratchett", age: 66, id: "3" }
+]; */
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        //console.log(parent)
+        //return _.find(authors, { id: parent.authorid });
+      }
+    }
   })
 });
 
 const AuthorType = new GraphQLObjectType({
-    name: "Author",
-    fields: () => ({
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      age: { type: GraphQLInt }
-    })
-  });
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        //return _.filter(books, {authorid: parent.id})
+      }
+    }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -47,19 +67,52 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(books, { id: args.id });
+        //return _.find(books, { id: args.id });
       }
     },
     author: {
-        type: AuthorType,
-        args: {id: {type: GraphQLID}},
-        resolve(parent, args){
-            return _.find(authors, {id: args.id})
-        }
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        //return books
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        //return authors
+      }
+    }
+  }
+});
+
+const Mutations = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        return author.save();
+      }
     }
   }
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutations
 });
--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 const express = require("express");
-const graphqlHTTP = require('express-graphql')
-const schema = require('./schema')
+const mongoose = require("mongoose");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+
+require("dotenv").config();
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}))
+  })
+);
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection success");
+});
 
 const PORT = process.env.PORT || 5000;
 
